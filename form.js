6c48fc5f3231677f2b41ac114610d33355d676a3
SalmanSabir41 <ss2885092@gmail.com>
2023-05-19 15:47:25 +0530

    add text

--- form.js
+++ form.js
@@ -91,7 +91,7 @@ function formSubmit() {
     }
 
     if (data1 != '' && data2 != '' && data3 != '' && data4 != '' && data5 != '' && data6 != '' && data7 != '' && (maleId || femaleId)) {
-        alert('form is submitted')
+        alert("form is submitted");
     }
 }
 
